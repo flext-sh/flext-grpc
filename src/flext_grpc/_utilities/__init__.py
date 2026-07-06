2868bbf6f7140155d792c719b6e3e0f82f59c9b1
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 00:21:20 -0300

    fixes

--- src/flext_grpc/_utilities/__init__.py
+++ src/flext_grpc/_utilities/__init__.py
@@ -8,9 +8,7 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_grpc._utilities.grpc import (
-        FlextGrpcUtilitiesGrpc as FlextGrpcUtilitiesGrpc,
-    )
+    from flext_grpc._utilities.grpc import FlextGrpcUtilitiesGrpc
 _LAZY_IMPORTS = build_lazy_import_map(
     {
         ".grpc": ("FlextGrpcUtilitiesGrpc",),
