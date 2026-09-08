94920bb5becf1d677eb6f00487bc41c4ab5cca90
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-08 01:34:48 -0300

    [WIP] wave-2 flext-grpc: import organization residue fixes

--- src/flext_grpc/utilities.py
+++ src/flext_grpc/utilities.py
@@ -6,7 +6,8 @@ from typing import override
 
 from flext_cli import u
 from flext_grpc import c, m, p, t
-from flext_grpc._utilities.grpc import FlextGrpcUtilitiesGrpc
+
+from ._utilities.grpc import FlextGrpcUtilitiesGrpc
 
 
 class FlextGrpcUtilities(u, FlextGrpcUtilitiesGrpc):
